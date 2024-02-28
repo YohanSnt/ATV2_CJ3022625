dd306ca3037912cc2c97a375938371b021ef4cd0
YohanSnt <yohan.s@aluno.ifsp.edu.br>
2024-02-28 11:14:42 -0300

    Exercícios 1 ao 10

--- ATV2_CJ3022625/Program.cs
+++ ATV2_CJ3022625/Program.cs
@@ -6,20 +6,101 @@
         {
             int reais = 1000, n, triplo, dobro, soma;
             float prcntg, inv, inv1, inv2, inv3, inv4, salario, ptg = 25, ptg1, s1;
-          
+
+            /*
+             
             //Exercício1
+            int m, cm, mm;
+            
+            Console.WriteLine("Valor em metros: ");
+            m=int.Parse(Console.ReadLine());
+            cm = m * 100;
+            Console.WriteLine("M em CM: {0} ", cm);
+            mm = m * 1000;
+            Console.WriteLine("M em MM: {0} ", mm);
            
-            Console.WriteLine("<<EX.1>>\n");
+           
+
+
+            //Exercício2
+            float f, c;
+           
+            Console.WriteLine("Valor em Fahrenheit: ");
+            f=float.Parse(Console.ReadLine());
+            c = (f - 32) * (5 / 9);
+            Console.WriteLine("Valor em Celcius = {0}", c);
+            Console.WriteLine("\nSempre resulta em 0 pois a fórmula tem de ser 9/5, e não 5/9.\nNesse caso:\n");
+            c = (f - 32) * (9 / 5);
+            Console.WriteLine("Valor correto em Celcius: {0}", c);
+
+            
+
+
+
+            //Exetcício3
+            float peso, altura,a1, imc;
+
+            Console.WriteLine("Diga seu peso: ");
+            peso=float.Parse(Console.ReadLine());
+            Console.WriteLine("Diga sua altura: ");
+            altura= float.Parse(Console.ReadLine());
+            a1 = altura * altura;
+            imc = (peso / a1);
+            Console.WriteLine("Seu IMC = {0}", imc);
+
+            
+
+
+            //Exercício4
+            float n1, n2, n3, m1, m2;
+
+            Console.WriteLine("Digite um número qualquer: ");
+            n1=float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número: ");
+            n2=float.Parse(Console.ReadLine());
+            Console.WriteLine("Outro número: ");
+            n3=float.Parse(Console.ReadLine());
+            m1 = n1 + n2 + n3;
+            m2 = m1 / 3;
+            Console.WriteLine("Média pondera: {0}", m2);
+
+           
+
+            //Exercício5
+            float s, t, v;
+
+            Console.WriteLine("<<Fórmula de Velocidade Média>>\n v = Δs/Δt");
+            Console.WriteLine("\nDigite a distância(variação de espaço): ");
+            s=float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o tempo que levou do início até o fim do movimento de variação: ");
+            t = float.Parse(Console.ReadLine());
+            v = s / t;
+            Console.WriteLine("Velocidade média do objeto = {0}", v);
+
+            
+
+            //Exercício6
             Console.WriteLine("\nDigite o salário: ");
             salario=float.Parse(Console.ReadLine());
             ptg1 = ptg / 100 * salario;
             s1 = salario + ptg1;
             Console.WriteLine("\nNovo salário com aumento de 25% = {0}", s1);
+           
+            */
 
-           //Exercício2
+            //Exercício7
+            float r, d, c;
+            Console.WriteLine("Digite o valor em real: ");
+            r=float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a cotação do dólar: ");
+            d=float.Parse(Console.ReadLine());
+            c = r * d;
+            Console.WriteLine("Conversão em dólar = {0}", c);
 
-            Console.WriteLine("\n<<EX.2>>\n");
-            Console.WriteLine("Digite um número: ");
+
+
+            //Exercício8
+            Console.WriteLine("\nDigite um número: ");
             n= int.Parse(Console.ReadLine());
             triplo = n * 3 + 1;
             Console.WriteLine("Sucessor do triplo de {0}: {1}",n, triplo);
@@ -29,10 +110,11 @@
             Console.WriteLine("Soma final: {0}", soma);
 
 
+            //Exercício9
 
-            //Exercício3
 
-            Console.WriteLine("\n<<EX.3>> \n");
+
+            //Exercício10
             Console.WriteLine("\nValor inicial: {0}\n", reais);
             Console.WriteLine("Primeiro dia, queda percentual: ");
             prcntg=float.Parse(Console.ReadLine());
@@ -54,6 +136,11 @@
             inv = prcntg / 100 * reais;
             inv4 = inv3 - inv;
             Console.WriteLine("Valor FINAL= {0}", inv4);
+
+            
+
+
+            //Exercício11
 
 
         }
